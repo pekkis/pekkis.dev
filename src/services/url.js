963265db1829311a33342b3291514d25610a31ae
Mikko Forsström <pekkisx@gmail.com>
2021-11-25 13:58:11 +0200

    Luspandor.

--- src/services/url.js
+++ src/services/url.js
@@ -1,6 +1,7 @@
 import { DateTime } from "luxon"
 
 export const blogPostUrl = post => {
-  const date = DateTime.fromISO(post.date)
-  return `/${date.toFormat("yyyy-LL-dd")}/${post.slug}`
+  const date = DateTime.fromISO(post.date).setZone("Europe/Helsinki")
+
+  return `/${datetoFormat("yyyy-LL-dd")}/${post.slug}`
 }
