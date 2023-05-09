d886088aa6eb357219199642faa6780aa780660d
Mikko Forsström <mikko.forsstrom@yle.fi>
2023-05-09 21:31:32 +0300

    Lazier laziness

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -4,6 +4,7 @@ import { FC } from "react";
 
 import { containerClass, innerClass } from "./VideoPlayer.css";
 import dynamic from "next/dynamic";
+import { useInView } from "react-intersection-observer";
 
 const ReactPlayer = dynamic(() => import("react-player"), {
   ssr: false,
@@ -17,15 +18,19 @@ type Props = {
 };
 
 const VideoPlayer: FC<Props> = ({ videoId }) => {
+  const { ref, inView } = useInView();
+
   return (
     <div className={containerClass}>
-      <div className={innerClass}>
-        <ReactPlayer
-          controls
-          width="100%"
-          height="100%"
-          url={`https://www.youtube.com/watch?v=${videoId}`}
-        />
+      <div ref={ref} className={innerClass}>
+        {inView && (
+          <ReactPlayer
+            controls
+            width="100%"
+            height="100%"
+            url={`https://www.youtube.com/watch?v=${videoId}`}
+          />
+        )}
       </div>
     </div>
   );
