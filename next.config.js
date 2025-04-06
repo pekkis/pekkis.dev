4d230d2cb44054028f7a4a4f224007b3da072830
Mikko Forsström <mikko.forsstrom@veikkaus.fi>
2025-04-06 18:45:46 +0300

    More independence

--- next.config.js
+++ next.config.js
@@ -13,6 +13,7 @@ const withBundleAnalyzer = bundleAnalyzer({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   reactStrictMode: true,
   typescript: {
     ignoreBuildErrors: false
