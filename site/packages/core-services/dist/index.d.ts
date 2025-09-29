cf1117286b00835de11b67a9bd62392b97f916bb
Mikko Forsström <mikko.forsstrom@veikkaus.fi>
2025-09-29 22:51:40 +0300

    Heps

--- site/packages/core-services/dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { ConfigUrls, UrlGeneratorInstance, UrlGeneratorInstanceDependencies } from './types';
-export type { ConfigUrls, UrlGeneratorInstance, UrlGeneratorInstanceDependencies, UrlOptions, ConfigUrlKey } from './types';
-export declare const createUrlGeneratorInstance: <TConfigUrls extends ConfigUrls>(dependencies: UrlGeneratorInstanceDependencies<TConfigUrls>) => UrlGeneratorInstance<TConfigUrls>;
-export default createUrlGeneratorInstance;
