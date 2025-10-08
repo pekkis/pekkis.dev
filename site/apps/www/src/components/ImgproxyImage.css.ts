62126ab0c154629475d469a69b666c8797db974c
Mikko Forsström <mikko.forsstrom@veikkaus.fi>
2025-10-08 19:50:37 +0300

    Lets try to fix layout shift

--- site/apps/www/src/components/ImgproxyImage.css.ts
+++ site/apps/www/src/components/ImgproxyImage.css.ts
@@ -1,14 +1,16 @@
 import { style } from "@vanilla-extract/css";
 
 export const containerClass = style({
-  width: "100%",
-  maxWidth: "100%"
+  // width: "100%",
+  // maxWidth: "100%",
+  // aspectRatio: "16 / 9"
   // backgroundColor: "rgb(0, 0, 0)"
 });
 
 export const imageClass = style({
   width: "100%",
   maxWidth: "100%",
-  display: "block"
+  display: "block",
+  aspectRatio: "16 / 9"
   // backgroundColor: "rgb(0, 0, 0)"
 });
